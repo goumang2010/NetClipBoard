400846507648e316ce22eae157e7c22fa8e26bea
勾芒 <goumang2010@live.com>
2016-01-08 00:23:04 +0800

    Add fetch note function.

--- QuickCopy/public/self/Index.Ajax.js
+++ QuickCopy/public/self/Index.Ajax.js
@@ -1,15 +1,29 @@
 function ShowQR(data) {
     var fetchurl = location.href;
+    var display = data;
     if (data != fetchurl) {
         fetchurl = fetchurl + "fetch?key=" + data;
     }
+    //If data is an address,then get the key only.
+    var dary = display.split("=");
+    if (dary.length > 1) {
+        display = dary[dary.length - 1];
+    }
     jQuery('#handCode').empty();
-    jQuery('#handCode').append("<a href='" + fetchurl + "'>" + data + "</a>");
+    jQuery('#handCode').append("<a href='" + fetchurl + "'>" + display + "</a>");
     jQuery('#qrcodeCanvas').empty();
     jQuery('#qrcodeCanvas').qrcode({
         label: "QR",
         text: fetchurl
     });
     // alert(data);
+}
+function fetchText(data) {
+    if (data != "") {
+        jQuery('#noteText').text(data);
+    }
+    function errHandle(err) {
+        alert(err);
+    }
 }
 //# sourceMappingURL=Index.Ajax.js.map
--- QuickCopy/public/self/Index.Ajax.ts
+++ QuickCopy/public/self/Index.Ajax.ts
@@ -1,18 +1,34 @@
 ﻿
 
 function ShowQR(data) {
-    var fetchurl: string  = location.href;
+    var fetchurl: string = location.href;
+    var display = (<string>data)
     if (data != fetchurl) {
         fetchurl = fetchurl+"fetch?key="+data
     }
+    //If data is an address,then get the key only.
+    var dary = display.split("=")
+    if (dary.length > 1) {
+        display = dary[dary.length - 1];
+    }
     jQuery('#handCode').empty();
-    jQuery('#handCode').append("<a href='"+fetchurl+"'>"+ data+"</a>");
+    jQuery('#handCode').append("<a href='"+fetchurl+"'>"+ display+"</a>");
     jQuery('#qrcodeCanvas').empty();
     jQuery('#qrcodeCanvas').qrcode({
         label:"QR",
         text: fetchurl
     	}); 
-
   // alert(data);
+}
+function fetchText(data) {
+    if (data != "") {
+        jQuery('#noteText').text(data);
+    // alert(data);
+    }
+
+    function errHandle(err: Error) {
+        alert(err);
+
+    }
 }
 
