c814d850677abfd95cd5e4b70a87b5d5cd50f89c
勾芒 <goumang2010@live.com>
2016-01-11 00:18:02 +0800

    Add copy command

--- QuickCopy/public/self/Index.Ajax.js
+++ QuickCopy/public/self/Index.Ajax.js
@@ -26,4 +26,30 @@ function fetchText(data) {
         alert(err);
     }
 }
+//The toolbar 4button
+function toLower() {
+    var orgstr = jQuery('#noteText').text();
+    jQuery('#noteText').text(orgstr.toLowerCase());
+}
+function toUpper() {
+    var orgstr = jQuery('#noteText').text();
+    jQuery('#noteText').text(orgstr.toUpperCase());
+}
+function copyToBoard() {
+    var textbox = jQuery('#noteText');
+    textbox.focus();
+    textbox.select();
+    var txt = textbox.text();
+    document.execCommand("copy");
+    //屏蔽类型检查
+    //var windowfake = <any>window;
+    //if (windowfake.clipboardData) {
+    //    windowfake.clipboardData.clearData();
+    //    windowfake.clipboardData.setData("Text", txt);
+    //    alert("已经成功复制到剪帖板上");
+    //}
+    //else {
+    //    alert("暂时无法复制到剪帖板上，已显示在上方，请自行复制，并在文件夹中打开！");
+    //}
+}
 //# sourceMappingURL=Index.Ajax.js.map
--- QuickCopy/public/self/Index.Ajax.ts
+++ QuickCopy/public/self/Index.Ajax.ts
@@ -17,7 +17,8 @@ function ShowQR(data) {
     jQuery('#qrcodeCanvas').qrcode({
         label:"QR",
         text: fetchurl
-    	}); 
+    }); 
+
   // alert(data);
 }
 function fetchText(data) {
@@ -31,4 +32,38 @@ function fetchText(data) {
 
     }
 }
+
+//The toolbar 4button
+function toLower() {
+    var orgstr: string = jQuery('#noteText').text();
+    jQuery('#noteText').text(orgstr.toLowerCase());
+
+}
+function toUpper() {
+    var orgstr: string = jQuery('#noteText').text();
+    jQuery('#noteText').text(orgstr.toUpperCase());
+
+}
+function copyToBoard() {
+    var textbox = jQuery('#noteText');
+    textbox.focus();
+    textbox.select();
+    var txt: string = textbox.text();
+    document.execCommand("copy");
+    //屏蔽类型检查
+    //var windowfake = <any>window;
+    //if (windowfake.clipboardData) {
+
+    //    windowfake.clipboardData.clearData();
+
+    //    windowfake.clipboardData.setData("Text", txt);
+
+    //    alert("已经成功复制到剪帖板上");
+
+    //}
+    //else {
+    //    alert("暂时无法复制到剪帖板上，已显示在上方，请自行复制，并在文件夹中打开！");
+    //}
+}
+
 
