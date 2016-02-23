029407b7f2a3a3e5437ceb43442db5b0eb5f7a46
goumang2010 <goumang2010@live.com>
2016-02-23 16:22:35 +0800

    migrate to express 4

--- QuickCopy/app.js
+++ QuickCopy/app.js
@@ -5,40 +5,31 @@ var socketctrl = require('./routes/socketctrl');
 var user = require('./routes/userctrl');
 //模块
 var socketio = require('socket.io');
-var http = require('http');
 var path = require('path');
 var mongoose = require('mongoose');
 var compression = require('compression');
-var MongoStore = require('connect-mongo')(express);
-var dbUrl = 'mongodb://localhost/netnote';
-mongoose.connect(dbUrl);
+//express 4
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var stylus = require('stylus');
 var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser());
-var stylus = require('stylus');
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(compression()); //use compression 
 app.use(stylus.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.session({
-    secret: 'netnote',
-    store: new MongoStore({
-        url: dbUrl,
-        collection: "sessions"
-    })
-}));
-app.use(app.router);
-// development only
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-}
+//app.use(app.router);
 app.get('/', routes.index);
 app.get('/about', routes.about);
 app.get('/contact', routes.contact);
@@ -52,10 +43,50 @@ app.get('/fetch', routes.fetch);
 app.post('/ajaxfetch', routes.ajaxfetch);
 //注册
 app.post('/user/signup', user.signup);
-var server = http.createServer(app);
-server.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
-//WebSocket处理
-socketio.listen(server).on('connection', socketctrl.socketlitener);
+// error handlers
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+var dbUrl = 'mongodb://localhost/netnote';
+var session = require('express-session');
+mongoose.connect(dbUrl, function (err) {
+    var MongoStore = require('connect-mongo')(session);
+    app.use(session({
+        secret: 'netnote',
+        store: new MongoStore({
+            url: dbUrl,
+            collection: "sessions",
+        }),
+        //https://github.com/expressjs/session/issues/56
+        saveUninitialized: true,
+        resave: true
+    }));
+    var server = app.listen(3000);
+    //WebSocket处理
+    socketio.listen(server).on('connection', socketctrl.socketlitener);
+});
+module.exports = app;
 //# sourceMappingURL=app.js.map
--- QuickCopy/app.ts
+++ QuickCopy/app.ts
@@ -5,13 +5,20 @@ import socketctrl = require('./routes/socketctrl');
 import user = require('./routes/userctrl');
 //模块
 import socketio = require('socket.io');
-import http = require('http');
 import path = require('path');
 import mongoose = require('mongoose');
 import compression = require('compression');
-const MongoStore = require('connect-mongo')(express);
-var dbUrl = 'mongodb://localhost/netnote'
-mongoose.connect(dbUrl)
+//express 4
+import logger = require('morgan');
+import bodyParser = require('body-parser');
+import cookieParser = require('cookie-parser');
+import favicon = require('serve-favicon');
+
+import stylus = require('stylus');
+import connect = require('connect');
+
+
+
 
 
 var app = express();
@@ -22,35 +29,24 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser());
 
-import stylus = require('stylus');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use(compression()); //use compression 
 app.use(stylus.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.session({
-    secret: 'netnote',
-    store: new MongoStore({
-        url: dbUrl,
-        collection: "sessions"
-    })
-}));
-app.use(app.router);
-// development only
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-    //db.on('error', function (error) {
-    //    console.log(error);
-    //}); 
-    //mongoose.set('debug', true);
-}
 
+//app.use(app.router);
 app.get('/', routes.index);
 app.get('/about', routes.about);
 app.get('/contact', routes.contact);
@@ -66,16 +62,62 @@ app.post('/ajaxfetch', routes.ajaxfetch);
 app.post('/user/signup', user.signup);
 
 
-var server = http.createServer(app);
+// catch 404 and forward to error handler
+app.use(function (req: express.Request, res: express.Response, next: express.NextFunction) {
+    var err:any = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
-server.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+var dbUrl = 'mongodb://localhost/netnote';
+import session = require('express-session');
+mongoose.connect(dbUrl, function (err: any) {
+   
+    const MongoStore = require('connect-mongo')(session);
+    app.use(session({
+        secret: 'netnote',
+        store: new MongoStore({
+            url: dbUrl,
+            collection: "sessions",
+        }),
+        //https://github.com/expressjs/session/issues/56
+        saveUninitialized: true,
+        resave: true
+    }));
+    var server = app.listen(3000);
+    //WebSocket处理
+    socketio.listen(server).on('connection', socketctrl.socketlitener);
 });
 
 
+module.exports = app;
 
-//WebSocket处理
-socketio.listen(server).on('connection', socketctrl.socketlitener);
+
+
 
 
 
